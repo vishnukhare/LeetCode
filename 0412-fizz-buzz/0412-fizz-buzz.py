9ccbaee89cb8bfb3699bcd1fdd8748794dbdb87c
Vishnu Khare <85063816+vishnukhare@users.noreply.github.com>
2025-08-17 11:13:38 +0530

    Time: 0 ms (100%), Space: 18.4 MB (42.47%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,4 +1,17 @@
 class Solution:
-  def fizzBuzz(self, n: int) -> list[str]:
-    d = {3: 'Fizz', 5: 'Buzz'}
-    return [''.join([d[k] for k in d if i % k == 0]) or str(i) for i in range(1, n + 1)]
+    def fizzBuzz(self, n: int) -> List[str]:
+        res = [0] * (n+1)
+        for i in range(0, n+1):
+            if i == 0:
+                continue
+            else:
+                if i % 3 == 0 and i % 5 == 0:
+                    res[i] = 'FizzBuzz'
+                elif i % 3 == 0:
+                    res[i] = 'Fizz'
+                elif i % 5 == 0:
+                    res[i] = 'Buzz'
+                else:
+                    res[i] = str(i)
+
+        return res[1:]

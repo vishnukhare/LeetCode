0871202ba8ad034f626569083ecc13c76e3092cd
Vishnu Khare <85063816+vishnukhare@users.noreply.github.com>
2025-09-20 17:53:45 +0530

    Time: 463 ms (95.82%), Space: 17.7 MB (96.01%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,44 @@
 class Solution:
-    def exist(self, board: list[list[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        
-        def dfs(r, c, k):
-            
-            if k == len(word):
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        visited = set()
+
+        def find_word(idx, r, c):
+            if idx == len(word):
                 return True
             
-            if not (0 <= r < m and 0 <= c < n and board[r][c] == word[k]):
+            if (
+                (r < 0 or r >=  rows)
+                or (c < 0 or c >= cols) 
+                or (word[idx] != board[r][c]) 
+                or (r, c) in visited
+            ):
                 return False
 
-            original_char = board[r][c]
-            board[r][c] = '#'
+            visited.add((r, c))
 
-            found = (dfs(r + 1, c, k + 1) or
-                     dfs(r - 1, c, k + 1) or
-                     dfs(r, c + 1, k + 1) or
-                     dfs(r, c - 1, k + 1))
+            res = (
+                find_word(idx+1, r+1, c) or
+                find_word(idx+1, r, c+1) or
+                find_word(idx+1, r-1, c) or
+                find_word(idx+1, r, c-1)
+            )
 
-            board[r][c] = original_char
-            
-            return found
+            visited.remove((r, c))
 
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == word[0]:
-                    if dfs(r, c, 0):
-                        return True
+            return res
         
+        count = {}
+        for c in word:
+            count[c] = 1 + count.get(c, 0)
+        
+        if count[word[0]] > count[word[-1]]:
+            word = word[::-1]
+        
+        for r in range(rows):
+            for c in range(cols):
+                if find_word(0, r, c):
+                    return True
+
         return False
